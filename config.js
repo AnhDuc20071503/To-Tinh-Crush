4af5c778706740c74a02b5b059b219e5c6723f01
AnhDuc20071503 <86760213+AnhDuc20071503@users.noreply.github.com>
2021-07-01 12:50:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: ' Bae à!',
+    introTitle: ' Bạn à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Đùa chút làm zì căng dữ vậy :)).',
+    messDesc: 'Đùa chút làm zì căng dữ vậy :.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' https://www.facebook.com/profile.php?id=100045696886342
+    messLink: 'http://fb.com' 
 }
